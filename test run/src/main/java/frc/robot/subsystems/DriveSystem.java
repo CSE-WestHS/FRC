4c57b5ef721090d0d80dbffdf52e05d779347c29
dcsdprogrammers <91615412+dcsdprogrammers@users.noreply.github.com>
2021-09-30 15:48:58 -0500

    new program
    New program

---
+++ test run/src/main/java/frc/robot/subsystems/DriveSystem.java
@@ -0,0 +1,43 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+import frc.robot.controls.OI;
+
+//A drive system.
+//this is what drives the robot with the controlers. 
+
+public class DriveSystem 
+{
+    /*
+    Remember, the PWNVictorSPX is the actual motor. 
+    PWMVictorSPX takes in the port number of the motor.
+    DifferentalDrive is a class that makes running the 2 motors together easier.
+    If you want to know more, consider reading the frc wpilib documentation to know more.
+    */
+    private static final DifferentialDrive wheelsMotor1 = new DifferentialDrive(new PWMVictorSPX(2), new PWMVictorSPX(1));
+    private static final DifferentialDrive wheelsMotor2 = new DifferentialDrive(new PWMVictorSPX(3),new PWMVictorSPX(0));
+    private static double leftSpeed = 0;
+    private static double rightSpeed = 0;
+    private static final double SPEED_MULTIPLIER = 1;
+
+    //Pretty obvious what this function does. Run this in the Robot.java teleopPeriodic() function to run the robot.
+    public static void update()
+    {
+        moveWheels(-OI.RIGHT_STICK.getY() *SPEED_MULTIPLIER, -OI.LEFT_STICK.getY() *SPEED_MULTIPLIER);
+    }
+
+    public static void moveWheels(double leftSpeed, double rightSpeed)
+    {
+        double _rightSpeed = -rightSpeed; //Set it to ngative to match the motor set up 
+        wheelsMotor2.tankDrive(leftSpeed, _rightSpeed);
+        wheelsMotor1.tankDrive(leftSpeed, _rightSpeed);
+    }
+
+    public static void stopWheels()
+    {
+        wheelsMotor2.stopMotor();
+        wheelsMotor1.stopMotor();
+    }
+}
