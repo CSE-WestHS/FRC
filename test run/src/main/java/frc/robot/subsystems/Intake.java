a78c830197a3b80db58275807784d173391aadd8
Eric-Trimble <91701520+Eric-Trimble@users.noreply.github.com>
2021-10-07 17:11:14 -0500

    fixed intake

--- test run/src/main/java/frc/robot/subsystems/Intake.java
+++ test run/src/main/java/frc/robot/subsystems/Intake.java
@@ -9,30 +9,30 @@ import frc.robot.controls.OI;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-public class BrosiusIsAMotor {
+public class Intake {
     private CANSparkMax broMo;
     private CANSparkMax broMo2;
 
-    public BrosiusIsAMotor() {
-        broMo = new CANSparkMax(2, MotorType.kBrushless);
-        broMo2 = new CANSparkMax(3, MotorType.kBrushless);
+    public Intake() {
+        inMotor1 = new CANSparkMax(2, MotorType.kBrushless);
+        inMotor2 = new CANSparkMax(3, MotorType.kBrushless);
        
-        broMo.set(0);
-        broMo2.set(0);
+        inMotor1.set(0);
+        inMotor2.set(0);
 
-        broMo.restoreFactoryDefaults();
-        broMo2.restoreFactoryDefaults();
+        inMotor1.restoreFactoryDefaults();
+        inMotor2.restoreFactoryDefaults();
     }
 
     public void update() {
         //activates intake when pressed
         if (OI.INTAKE_BUTTON.isHold()) {
-            broMo.set(.15);
-            broMo2.set(.15);
+            inMotor1.set(.15);
+            inMotor2.set(.15);
             System.out.println("intake button pressed");
         } else {
-            broMo.set(0);
-            broMo2.set(0);
+            inMotor1.set(0);
+            inMotor2.set(0);
         }
     }
 }
