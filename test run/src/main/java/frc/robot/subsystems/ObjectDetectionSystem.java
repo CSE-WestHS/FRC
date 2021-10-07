9e08f5a73f186249ced4219ce640cb0105930c37
Eric-Trimble <91701520+Eric-Trimble@users.noreply.github.com>
2021-10-07 17:06:19 -0500

    object detection system wip

--- test run/src/main/java/frc/robot/subsystems/ObjectDetectionSystem.java
+++ test run/src/main/java/frc/robot/subsystems/ObjectDetectionSystem.java
@@ -24,15 +24,37 @@ public class ObjectDetectionsystem {
     public void update() {
 
         NetworkTable table = 
-NetworkTableInstance.getDefault().getTable("limelight");
-    NetworkTableEntry tx = table.getEntry("tx")
-    NetworkTableEntry ty = table.getEntry("ty")
-    NetworkTableEntry ta = table.getEntry("ta")
-    NetworkTableEntry tv = table.getEntry("tv")
-    NetworkTableEntry tShort = table.getentry("tshort")
-    NetworkTableEntry tlong = table.getEntry("tlong")
+        NetworkTableInstance.getDefault().getTable("limelight");
+        NetworkTableEntry tx = table.getEntry("tx")
+        NetworkTableEntry ty = table.getEntry("ty")
+        NetworkTableEntry ta = table.getEntry("ta")
+        NetworkTableEntry tv = table.getEntry("tv")
+        NetworkTableEntry tShort = table.getentry("tshort")
+        NetworkTableEntry tlong = table.getEntry("tlong")
 
     //read values periodically
+    xDistFromCH = tx.getDouble(0.0);
+    yDistFromCH = ty.getDouble(0.0);
+    area = ta.getDouble(0.0);
+    validTrget = tv.getDouble(0.0);
+    tShort = tshort.getDouble(0.0);
+    tLong = tlong.getDouble(0.0);
 
+    Debug.printOnce("LimeLightX: " + xDistFromCH +
+    "\n LimeLightY: " + yDistFromCh +
+    "\n Target Area %: " + area +
+    "\n ValidTarget: " + validTarget +
+    "\n Short Side of Yellow Box: " +
+    "\n");
+
+    //uncomment to make robot move while seeing ball.
+    if (xDistFromCH >= -12 && xDistFromCH  <= 12 &&
+        area <= 3 && area >= 0.03 &&
+        validTarget == 1.0) {
+        System.out.printIn("Target outside of bounds. \n");
+        }
+        if (xDistFromCH > turnDifference && validTarget == 1.0) {
+        ballPosition = "Left"; 
+        }
     }
 }
