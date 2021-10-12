be0567612010bc52c6cabdee90f7f74ec990c7c6
Mr. Massey <91700279+Matthew-Caldwell@users.noreply.github.com>
2021-10-12 17:23:23 -0500

    deadzone 5000

--- test run/src/main/java/frc/robot/subsystems/DriveSystem.java
+++ test run/src/main/java/frc/robot/subsystems/DriveSystem.java
@@ -25,7 +25,7 @@ public class DriveSystem
     //Pretty obvious what this function does. Run this in the Robot.java teleopPeriodic() function to run the robot.
     public static void update()
     {
-        if (-OI.LEFT_STICK.getY() >= 20000) 
+        if (-OI.LEFT_STICK.getY() >= 5000) 
         {
              moveWheels(-OI.RIGHT_STICK.getY() *SPEED_MULTIPLIER, -OI.LEFT_STICK.getY() *SPEED_MULTIPLIER);
         }
