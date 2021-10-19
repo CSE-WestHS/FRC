b294fed4df8bfee213a34053d71e31b6c037aa09
Mr. Massey <91700279+Matthew-Caldwell@users.noreply.github.com>
2021-10-19 18:49:32 -0500

    Update OI.java

--- test run/src/main/java/frc/robot/controls/OI.java
+++ test run/src/main/java/frc/robot/controls/OI.java
@@ -21,8 +21,8 @@ public static final Joystick RIGHT_STICK = new Joystick(1);
 // Controls what the pneumatic control buttons control... 
 //public static final Button NEXT_BUTTON = new Button(COLORSTICK, 5);
 //public static final Trigger PREVIOUS_BUTTON = new Trigger(COLORSTICK, 2);
-//public static final Button PNEU_FOREWARD_BUTTON = new Button(COLOR_STICK, 6);
-//public static final Button PNEU_BACKWARD_BUTTON = new Button(COLOR_STICK, 4);
+public static final Button PNEU_FOREWARD_BUTTON = new Button(RIGHT_STICK, 7);
+public static final Button PNEU_BACKWARD_BUTTON = new Button(RIGHT_STICK, 8);
  
 public static final Button INTAKE_BUTTON = new Button(RIGHT_STICK, 2);
 public static final Button SHOOT_BUTTON_LOW = new Button(LEFT_STICK, 4);
