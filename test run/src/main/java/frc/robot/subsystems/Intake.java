86333f8b0b9608db1f09d31e546d60665121cc93
GWS2007 <91700099+GWS2007@users.noreply.github.com>
2021-10-21 14:54:37 -0500

    Update Intake.java

--- test run/src/main/java/frc/robot/subsystems/Intake.java
+++ test run/src/main/java/frc/robot/subsystems/Intake.java
@@ -5,17 +5,14 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.SpeedController;
 import frc.robot.util.Debug;
 import frc.robot.controls.OI;
-
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-public class Intake 
-{
+public class Intake {
     private static CANSparkMax inMotor1;
     private static CANSparkMax inMotor2;
 
-    public Intake() 
-    {
+    public Intake() {
         inMotor1 = new CANSparkMax(2, MotorType.kBrushless);
         inMotor2 = new CANSparkMax(3, MotorType.kBrushless);
        
@@ -25,18 +22,16 @@ public class Intake
         inMotor1.restoreFactoryDefaults();
         inMotor2.restoreFactoryDefaults();
     }
-
     public static void update() 
     {
         //activates intake when pressed
-      //  if (OI.INTAKE_BUTTON.isHold()) {
+        if (OI.INTAKE_BUTTON.isHold()) {
             inMotor1.set(.15);
-            inMotor2.set(.15);
-       // } 
-      //  else 
-      //  {
-      //      inMotor1.set(0);
-      //      inMotor2.set(0);
-      //  }
-    }
+            inMotor2.set(.15);  
+        } 
+        else {
+            inMotor1.set(0);
+            inMotor2.set(0);    
+        }   
+    }   
 }
