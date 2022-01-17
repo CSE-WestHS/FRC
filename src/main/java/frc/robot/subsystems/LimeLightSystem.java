5fa2f9b3e180ab6c4bdd52f7b184d8d1c240765b
Zandar-Double07 <91700064+Zandar-Double07@users.noreply.github.com>
2022-01-17 14:02:37 -0600

    Added getter methhods in the limelight

---
+++ src/main/java/frc/robot/subsystems/LimeLightSystem.java
@@ -0,0 +1,39 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+public class LimeLightSystem {
+    private static NetworkTable table;
+    private static NetworkTableEntry tx;
+    private static NetworkTableEntry ty;
+    private static NetworkTableEntry ta;
+
+    public LimeLightSystem(){
+       table = NetworkTableInstance.getDefault().getTable("limelight");
+    }
+    public void start(){
+        tx = table.getEntry("tx");
+        ty = table.getEntry("ty");
+        ta = table.getEntry("ta");
+
+        SmartDashboard.putNumber("LimelightX", tx.getDouble(0.0));
+        SmartDashboard.putNumber("LimelightY", ty.getDouble(0.0));
+        SmartDashboard.putNumber("LimelightArea", ta.getDouble(0.0));
+    }
+    public double getX()
+    {
+        return table.getEntry("tx").getDouble(0.0);
+    }
+    public double getY()
+    {
+        return table.getEntry("ty").getDouble(0.0);
+    }
+    public double getArea()
+    {
+        return table.getEntry("ta").getDouble(0.0);
+    }
+}
