d7ca1fc867254bda05a1a64ba1a73f97e13b0356
Zandar-Double07 <91700064+Zandar-Double07@users.noreply.github.com>
2022-01-18 17:24:01 -0600

    Added import for limilight system Added limits  to distance to power method

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,5 +1,5 @@
 package frc.robot.subsystems;
-
+import frc.robot.subsystems.LimeLightSystem;
 import frc.robot.controls.OI;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -27,15 +27,37 @@ public class Shooter {
      * 
      * @param power = -100 to 100
      */
-    public static void setPower(double power) {
+    public static void setPower(double distance) {
         // Distance will be a numebr given to us by the limelight
         // the equation given will be updated to make parameters from -1 to 1
         // The mins and maxes will not be -1 and 1, this is too much power on the motors
 
-        double motorSpeed = power / 100.0;
+        double motorSpeed = distance / 100.0;
         if (OI.SHOOT_BUTTON.isHold()) {
-            shootMotor1.set(motorSpeed);
-            shootMotor2.set(motorSpeed);
+            /*This if statement keeps the code from giving a larger
+            value than 1, which is the largest value the set command
+            can take
+            */
+            
+            if (motorSpeed > 1)
+            {
+                shootMotor1.set(1);
+                shootMotor2.set(-1);
+            }
+             /*This if statement keeps the code from giving a smaller
+            value than -1, which is the smallest value the set command
+            can take
+            */
+            else if ( motorSpeed < -1)  
+            {
+                shootMotor1.set(-1);
+                shootMotor2.set(1);
+            }
+            else {
+                shootMotor1.set(motorSpeed);
+                shootMotor2.set(motorSpeed);
+            }
+
         } else {
             shootMotor1.set(0);
             shootMotor2.set(0);
@@ -44,5 +66,7 @@ public class Shooter {
 
     public static void limelightAdjust() {
         // TODO be implemennted after limelight is implemented
+        //Take in value from lidar
+        //
     }
 }
