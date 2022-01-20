893952f12c9ce9e63754e8e988b7af465c1762de
Zandar-Double07 <91700064+Zandar-Double07@users.noreply.github.com>
2022-01-20 15:59:26 -0600

    Trying to fix conflicts between 2 files

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,6 +1,6 @@
 package frc.robot.subsystems;
 
-import frc.robot.subsystems.LimeLightSystem;
+//import frc.robot.subsystems.LimeLightSystem;
 import frc.robot.controls.OI;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -15,7 +15,6 @@ to work. We will put in calculations for distance
 public class Shooter {
     public static CANSparkMax shootMotor1 = new CANSparkMax(5, MotorType.kBrushless);;
     public static CANSparkMax shootMotor2 = new CANSparkMax(6, MotorType.kBrushless);;
-    public static double limeToPower = 0.5;
 
     public Shooter() {
         shootMotor1.setInverted(true);
@@ -41,13 +40,18 @@ public class Shooter {
         }
     }
 
-    // This method is used in the shooting mechanism
+    // This method's main use is in the shooting mechanism
+    /*
+     * This method takes in a distance reading from the LIDAR
+     * Sensor and converts that into a power based on the reading
+     */
     public static double getPower() {
         double distanceCM;
         double power;
         // LIDARSensor file needs to be added
         // sets distance equal to the reading of the Lidar sensor in cm
-        distanceCM = LIDARSensor.getDistance();
+        // distances may be updated to feet
+        distanceCM = 100.0;// LIDARSensor.getDistance();
         // if the reading is between 0 and 1 meter
         if (distanceCM >= 0 && distanceCM <= 100) {
             power = 0.2;
