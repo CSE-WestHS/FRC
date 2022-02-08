fce2e64067b9310e76f0cbe15395d46b5134fa5d
Jeff Gordon <jfgordon2@msn.com>
2022-02-08 16:55:30 -0600

    merge correction

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,7 +9,6 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.subsystems.LIDARSensor;
 import frc.robot.subsystems.DriveSystem;
 import frc.robot.subsystems.LIDARSensor;
 import frc.robot.subsystems.LimeLightSystem;
