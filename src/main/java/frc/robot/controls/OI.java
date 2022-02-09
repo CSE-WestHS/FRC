b87914345e132dcb12222f200c0eda42927ae409
Zandar-Double07 <91700064+Zandar-Double07@users.noreply.github.com>
2022-02-08 19:35:01 -0600

    Deleted unused files, Updated button mapping for drivers

--- src/main/java/frc/robot/controls/OI.java
+++ src/main/java/frc/robot/controls/OI.java
@@ -5,7 +5,8 @@ import edu.wpi.first.wpilibj.Joystick;
 
 //OI class: A control system class
 //OI class checks control inputs such as joysticks and buttons and update the variables accordingly.
-// You will find this class being used essentially everywhere (unless it is not).
+// You will find this class being used essentially everywhere that you control with joysticks 
+//(exg. what joysticks are used for driving).
 
 public class OI {
     // Joysticks takes in the port number
@@ -13,10 +14,9 @@ public class OI {
     public static final Joystick LEFT_STICK = new Joystick(0);
     public static final Joystick RIGHT_STICK = new Joystick(1);
 
-    public static final Button INTAKE_BUTTON = new Button(LEFT_STICK, 5);
-    public static final Button SPITOUT_BUTTON = new Button(LEFT_STICK, 6);
-    public static final Button SHOOT_BUTTON = new Button(RIGHT_STICK, 7);
-
+    public static final Button INTAKE_BUTTON = new Button(LEFT_STICK, 2);
+    public static final Button SPITOUT_BUTTON = new Button(LEFT_STICK, 3);
+    public static final Button SHOOT_BUTTON = new Button(LEFT_STICK, 4);
     // Since index starts at 0, using the codes above as index needs to subtract 1
     private static Button[] buttons = new Button[] {
             INTAKE_BUTTON,
@@ -24,18 +24,7 @@ public class OI {
             SHOOT_BUTTON,
 
     };
-
-    // Getting the inputs of the joystick and update the variables
     public static void update() {
-        // if (NEXT_BUTTON.isPressed)
-        // {
-        // ControlModule.changeMode(false);
-        // }
-        // else if(PREVIOUS_BUTTON.isPressed)
-        // {
-        // ControlModule.changeMode(true);
-        // }
-        // System.out.println(COLOR_STICK.getTrigger());
         for (int i = 0; i < buttons.length; i++) {
             buttons[i].setStates();
         }
