99e315aada1f4ca02606a9428aa8ee777fc539ef
Zandar-Double07 <91700064+Zandar-Double07@users.noreply.github.com>
2022-11-12 11:19:03 -0600

    updated buttons

--- src/main/java/frc/robot/controls/OperatorInput.java
+++ src/main/java/frc/robot/controls/OperatorInput.java
@@ -6,21 +6,18 @@ import edu.wpi.first.wpilibj.XboxController;
 
 
 public class OperatorInput {
-    public static final XboxController controller = new XboxController(0);
+    public static final XboxController controller = new XboxController(1);
     // There is no Button 0, any buttons assigned to 0 are in fact unbound.
-    public static final Button elevatorButton = new Button(controller, 0);//Elavator up? Unassigned
-    public static final Button elevatorSpitoutbutton = new Button(controller, 5);//Elavator down?
-    public static final Button intakeButton = new Button(controller, 0);//Reassign to RT
-    public static final Button shootLowGoalButton = new Button(controller, 0);//reassigned to POV
-    public static final Button shootHighGoalButton = new Button(controller, 0);//Reassigned to POV
-    public static final Button spitoutButton = new Button(controller, 6);
-    public static final Button liftButton = new Button(controller, 2);
-    public static final Button lowerButton = new Button(controller, 1);
-    public static final Button adjustButton = new Button(controller, 8);
-    public static final Button adjustButtonTurn = new Button(controller, 0);
-    public static final Button adjustButtonDistance = new Button(controller, 0);
-
-
+    public static final Button elevatorButton = new Button(controller, 1);//Elavator up? Unassigned
+    public static final Button elevatorSpitoutbutton = new Button(controller, 6);//Elavator down?
+   // public static final Button intakeButton = new Button(controller, 1);//Reassign to RT
+    //public static final Button shootLowGoalButton = new Button(controller, 1);//reassigned to POV
+    //public static final Button shootHighGoalButton = new Button(controller, 1);//Reassigned to POV
+    public static final Button spitoutButton = new Button(controller, 7);
+    public static final Button liftButton = new Button(controller, 3);
+    public static final Button lowerButton = new Button(controller, 2);
+    public static final Button adjustButton = new Button(controller, 9);
+    
     
 }
 
